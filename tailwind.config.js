a2e59c21e9ebcaded13e1e809d9b2168d71f5a81
g30ffr3ll1n <grellin123@gmail.com>
2023-02-13 18:33:54 +0800

    :construction: added package, updated configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.vue'],
   theme: {
     extend: {},
+    screens: {
+      '2xs': '350px',
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+      cursor: ['disabled'],
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
