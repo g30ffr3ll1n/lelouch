fe44eed0f184e739358a514b198e87e73c942e97
g30ffr3ll1n <grellin123@gmail.com>
2023-02-13 08:43:47 +0800

    :tada: initial setup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+html {
+  @apply bg-gray-50;
+}
+
+html,
+body,
+#app {
+  @apply h-full;
+}
+</style>
---
+++ src/components/NavComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="nav" class="h-12">
+        <!-- nav to: using path -->
+        <router-link to="/home">Home</router-link> 
+        <router-link to="/about">About</router-link> 
+
+        <!-- nav to: using name -->
+        <!-- <router-link :to="{ name: 'HomePage' }">Home</router-link> |
+        <router-link :to="{ name: 'AboutPage' }">About</router-link> -->
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+    name: 'NavComponent',
+    methods: {
+        ...mapActions('auth', {
+            logoutUser: 'logout'
+        }),
+    }
+}
+</script>
